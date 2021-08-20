4992f297b68e6058254636e234e36009fd99dd72
GeraldFigueroa <emm.gomez@pm.me>
2021-08-20 10:55:04 -0600

    Agregando link del repositorio

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
             <p>Created by <div> Gerald Figueroa</div></p>
             <a href="https://www.coingecko.com/en/api/documentation" target="_blank">Crypto Monedas API</a>
             <a href="https://exchangeratesapi.io/" target="_blank">Divisas API</a>
-            <a href="">GitHub</a>
+            <a href="https://github.com/GeraldFigueroa/Crypto.Info">GitHub</a>
         </div>
     )
 }
